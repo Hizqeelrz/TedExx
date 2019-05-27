bbe4d665bbe2a776dd4baabd19db35cc220fec66
hizqeelrz <hizqeel1214@gmail.com>
2019-05-27 22:00:57 +0500

    update

--- jsCompiler.js
+++ jsCompiler.js
@@ -97,11 +97,226 @@ function tokenizer(input) {
 
 }
 
-function compiler(input) {
-  let tokens = tokenizer(input);
+// Parser 
+
+function parser(tokens) {
+  // current variable will be used as cursor
+  let current = 0;
+
+  function walk() {
+
+    let token = tokens[current];
+
+    if (token.type === 'number') {
+      current++;
+
+      return {
+        type: "NumberLiteral",
+        value: token.value,
+      };
+    }
+
+    if (token.type === 'string') {
+      current++;
+
+      return {
+        type: 'StringLiteral',
+        value: token.value,
+      };
+    }
+
+    if (
+      token.type === 'paren' &&
+      token.value === '('
+    ){
+
+      token = tokens[++current];
+
+      let node = {
+        type: "CallExpression",
+        name: token.value,
+        params: [],
+      };
+
+      token = tokens[+current];
+
+      while (
+        (token.type !== 'paren') ||
+        (token.type === 'paren' && token.value !== ')')
+      ) {
+        node.params.push(walk());
+        token = tokens[current];
+      }
+
+      current++;
+
+      return node;
+    }
+    throw new TypeError(token.type);
+  }
+
+  let ast = {
+    type: 'Program',
+    body: [],
+  };
+
+  while (current < tokens.length) {
+    ast.body.push(walk());
+  }
+  return ast;
+
 }
 
+// traverser
+
+function traverser(ast, visitor) {
+
+  function traverserArray(array, parent) {
+    array.forEach(child => {
+      traverseNode(child, parent);
+    });
+  }
+
+  function traverseNode(node, parent) {
+    let methods = visitor[node.type];
+
+    if (methods && methods.enter) {
+      methods.enter(node, parent);
+    }
+
+    switch (node.type) {
+      case "Program":
+        traverserArray(node.body, node);
+        break;
+      case "CallExpression":
+        traverserArray(node.params, node);
+        break;
+
+      case "NumberLiteral":
+      case "StringLiteral":
+        break;
+
+      default:
+        throw new TypeError(node.type);
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+  }
+
+  traverseNode(ast, null);
+
+}
+
+// transformer
+function transformer(ast) {
+  let newAst = {
+    type: "Program",
+    body: [],
+  };
+
+
+  ast._context = newASt.body;
+
+  traverser(ast, {
+    NumberLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: "StringLiteral",
+          value: node.value,
+        });
+      },
+    },
+
+
+    CallExpression: {
+      enter(node, parent) {
+        let expression = {
+          type: "callExpression",
+          callee: {
+            type: "Identifier",
+            name: node.name,
+          },
+          arguments: [],
+        };
+
+        node._context = expression.arguments;
+
+        if (parent.type !== "CallExpression") {
+
+          expression = {
+            type: "ExpressionStatement",
+            expression: expression,
+          };
+        }
+
+        parent._context.push(expression);
+      },
+    }
+  });
+
+  return newAst;
+}
+
+function codeGenerator(node) {
+
+  switch (node.type) {
+
+    case 'Program':
+      return node.body.map(codeGenerator)
+        .join('\n');
+
+    case 'ExpressionStatement':
+      return (
+        codeGenerator(node.expression) +
+        ';' // << (...because we like to code the *correct* way)
+      );
+    case 'CallExpression':
+      return (
+        codeGenerator(node.callee) +
+        '(' +
+        node.arguments.map(codeGenerator)
+          .join(', ') +
+        ')'
+      );
+
+    // For `Identifier` we'll just return the `node`'s name.
+    case 'Identifier':
+      return node.name;
+
+    // For `NumberLiteral` we'll just return the `node`'s value.
+    case 'NumberLiteral':
+      return node.value;
+
+    // For `StringLiteral` we'll add quotations around the `node`'s value.
+    case 'StringLiteral':
+      return '"' + node.value + '"';
+
+    // And if we haven't recognized the node, we'll throw an error.
+    default:
+      throw new TypeError(node.type);
+  }
+}
+
+
+function compiler(input) {
+  let tokens = tokenizer(input);
+  let ast    = parser(tokens);
+  let newAst = transformer(ast);
+  let output = codeGenerator(newAst);
+
+  // and simply return the output!
+  return output;
+}
+
+
+// Now I'm just exporting everything...
 module.exports = {
   tokenizer,
+  parser,
+  traverser,
+  transformer,
+  codeGenerator,
   compiler,
 };
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const {
   tokenizer,
+  parser,
+  transformer,
+  codeGenerator,
   compiler,
 } = require('./jsCompiler');
-
 const assert = require('assert');
 
 var readline = require('readline-sync');
@@ -18,15 +20,79 @@ var input = readline.question("Write any Valid LISP Expression to evaluate \n\n"
 
 console.log("\n");
 
+// INPUT String MATCH POINT
+
 switch(true) {
   case input === '(add 2 (subtract 4 2))':
     console.log("Passed");
     break;
+  case input === '(add 2 (add 4 2))':
+    console.log("Passed");
+    break;
+  case input === '(add 2 (multiply 4 2))':
+    console.log("Passed");
+    break;
+  case input === '(add 2 (divide 4 2))':
+    console.log("Passed")
+    break;
+  case input == '(subtract 3 (multiply 2 7))':
+    console.log("Passed")
+    break;
+  case input == '(multiply 7 (divide 8 2))':
+    console.log("Passed")
+    break;
+  case input == '(Multiply 9 (Add 3 2))':
+    console.log("Passed")
+    break;  
+  // case input == '(add 2 (divide 4 2))':
+  //   console.log("Accepted")
+  //   break;
+  // case input == '(add 2 (divide 4 2))':
+  //   console.log("Accepted")
+  //   break;
+  // case input == '(add 2 (divide 4 2))':
+  //   console.log("Accepted")
+  //   break;
+  // case input == '(add 2 (divide 4 2))':
+  //   console.log("Accepted")
+  //   break;
+  // case input == '(add 2 (divide 4 2))':
+  //   console.log("Accepted")
+  //   break;  
   default:
     console.log("This String is Rejected \n");
     console.log("Error is Displayed Below \n");
     break;
 }
+
+// ----------------------- OUTPUT MATCH POINT ----------------------- //
+
+switch(true) {
+  case input == '(add 2 (subtract 4 2))':
+    var output = 'add(2, subtract(4, 2));';
+    break;
+  case input == '(add 2 (add 4 2))':
+    var output = 'add(2, add(4, 2));';
+    break;
+  case input == '(add 2 (divide 4 2))':
+    var output = 'add(2, divide(4, 2));';
+    break;
+  case input == '(add 2 (multiply 4 2))':
+    var output = 'add(2, multiply(4, 2));';
+    break;
+  case input == '(subtract 3 (multiply 2 7))':
+    var output = 'subtract(3, multiply(2, 7));';
+    break;
+  case input == '(multiply 7 (divide 8 2))':
+    var output = 'multiply(7, divide(8, 2));';
+    break;
+  case input == '(Multiply 9 (Add 3 2))':
+    var output = 'Multiply(9, Add(3, 2));';
+    break;  
+}
+
+
+// ----------------------- TOKENIZER ----------------------- //
 
 if (input == '(add 2 (subtract 4 2))') {
   var tokens =
@@ -42,8 +108,476 @@ if (input == '(add 2 (subtract 4 2))') {
       { type: 'paren',  value: ')'        }
     ];
 
+  } if (input == '(add 2 (add 4 2))') {
+    var tokens =
+    [
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'add'      },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'add' },
+      { type: 'number', value: '4'        },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: ')'        },
+      { type: 'paren',  value: ')'        }
+    ];
+  } if (input == '(add 2 (divide 4 2))') {
+    var tokens =
+    [
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'add'      },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'divide' },
+      { type: 'number', value: '4'        },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: ')'        },
+      { type: 'paren',  value: ')'        }
+    ];
+  }  if (input == '(add 2 (multiply 4 2))') {
+    var tokens =
+    [
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'add'      },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'multiply' },
+      { type: 'number', value: '4'        },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: ')'        },
+      { type: 'paren',  value: ')'        }
+    ];
+  } if (input == '(subtract 3 (multiply 2 7))') {
+    var tokens =
+    [
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'subtract'      },
+      { type: 'number', value: '3'        },
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'multiply' },
+      { type: 'number', value: '2'        },
+      { type: 'number', value: '7'        },
+      { type: 'paren',  value: ')'        },
+      { type: 'paren',  value: ')'        }
+    ];
+  } if (input == '(multiply 7 (divide 8 2))') {
+    var tokens =
+    [
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'multiply'      },
+      { type: 'number', value: '7'        },
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'divide' },
+      { type: 'number', value: '8'        },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: ')'        },
+      { type: 'paren',  value: ')'        }
+    ];
+  } if (input == '(Multiply 9 (Add 3 2))') {
+    var tokens =
+    [
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'Multiply'      },
+      { type: 'number', value: '9'        },
+      { type: 'paren',  value: '('        },
+      { type: 'name',   value: 'Add' },
+      { type: 'number', value: '3'        },
+      { type: 'number', value: '2'        },
+      { type: 'paren',  value: ')'        },
+      { type: 'paren',  value: ')'        }
+    ];
   }
 
+
+// ----------------------- AST PARSER ----------------------- //
+
+if (input == '(add 2 (subtract 4 2))') {
+var ast = {
+  type: 'Program',
+  body: [{
+    type: 'CallExpression',
+    name: 'add',
+    params: [{
+      type: 'NumberLiteral',
+      value: '2'
+    }, {
+      type: 'CallExpression',
+      name: 'subtract',
+      params: [{
+        type: 'NumberLiteral',
+        value: '4'
+      }, {
+        type: 'NumberLiteral',
+        value: '2'
+      }]
+    }]
+  }]
+};
+
+} if (input == '(add 2 (add 4 2))') {
+  var ast = {
+    type: 'Program',
+    body: [{
+      type: 'CallExpression',
+      name: 'add',
+      params: [{
+        type: 'NumberLiteral',
+        value: '2'
+      }, {
+        type: 'CallExpression',
+        name: 'add',
+        params: [{
+          type: 'NumberLiteral',
+          value: '4'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }]
+  };
+} if (input == '(add 2 (divide 4 2))') {
+  var ast = {
+    type: 'Program',
+    body: [{
+      type: 'CallExpression',
+      name: 'add',
+      params: [{
+        type: 'NumberLiteral',
+        value: '2'
+      }, {
+        type: 'CallExpression',
+        name: 'divide',
+        params: [{
+          type: 'NumberLiteral',
+          value: '4'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }]
+  };
+} if (input == '(add 2 (multiply 4 2))') {
+  var ast = {
+    type: 'Program',
+    body: [{
+      type: 'CallExpression',
+      name: 'add',
+      params: [{
+        type: 'NumberLiteral',
+        value: '2'
+      }, {
+        type: 'CallExpression',
+        name: 'multiply',
+        params: [{
+          type: 'NumberLiteral',
+          value: '4'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }]
+  };
+} if (input == '(subtract 3 (multiply 2 7))') {
+  var ast = {
+    type: 'Program',
+    body: [{
+      type: 'CallExpression',
+      name: 'subtract',
+      params: [{
+        type: 'NumberLiteral',
+        value: '3'
+      }, {
+        type: 'CallExpression',
+        name: 'multiply',
+        params: [{
+          type: 'NumberLiteral',
+          value: '2'
+        }, {
+          type: 'NumberLiteral',
+          value: '7'
+        }]
+      }]
+    }]
+  };
+} if (input == '(multiply 7 (divide 8 2))') {
+  var ast = {
+    type: 'Program',
+    body: [{
+      type: 'CallExpression',
+      name: 'multiply',
+      params: [{
+        type: 'NumberLiteral',
+        value: '7'
+      }, {
+        type: 'CallExpression',
+        name: 'divide',
+        params: [{
+          type: 'NumberLiteral',
+          value: '8'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }]
+  };
+} if (input == '(Multiply 9 (Add 3 2))') {
+  var ast = {
+    type: 'Program',
+    body: [{
+      type: 'CallExpression',
+      name: 'Multiply',
+      params: [{
+        type: 'NumberLiteral',
+        value: '9'
+      }, {
+        type: 'CallExpression',
+        name: 'Add',
+        params: [{
+          type: 'NumberLiteral',
+          value: '3'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }]
+  };
+}
+
+
+// ----------------------- NEWAST PARSER TRANSFORMATION ----------------------- //
+
+if (input == '(add 2 (subtract 4 2))') {
+var newAst = {
+  type: 'Program',
+  body: [{
+    type: 'ExpressionStatement',
+    expression: {
+      type: 'CallExpression',
+      callee: {
+        type: 'Identifier',
+        name: 'add'
+      },
+      arguments: [{
+        type: 'NumberLiteral',
+        value: '2'
+      }, {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'subtract'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '4'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }
+  }]
+};
+
+} if (input == '(add 2 (add 4 2))') {
+  var newAst = {
+  type: 'Program',
+  body: [{
+    type: 'ExpressionStatement',
+    expression: {
+      type: 'CallExpression',
+      callee: {
+        type: 'Identifier',
+        name: 'add'
+      },
+      arguments: [{
+        type: 'NumberLiteral',
+        value: '2'
+      }, {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'add'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '4'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }
+  }]
+};
+} if (input == '(add 2 (divide 4 2))') {
+  var newAst = {
+    type: 'Program',
+    body: [{
+      type: 'ExpressionStatement',
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'add'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '2'
+        }, {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'divide'
+          },
+          arguments: [{
+            type: 'NumberLiteral',
+            value: '4'
+          }, {
+            type: 'NumberLiteral',
+            value: '2'
+          }]
+        }]
+      }
+    }]
+  };
+} if (input == '(add 2 (multiply 4 2))') {
+  var newAst = {
+    type: 'Program',
+    body: [{
+      type: 'ExpressionStatement',
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'add'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '2'
+        }, {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'multiply'
+          },
+          arguments: [{
+            type: 'NumberLiteral',
+            value: '4'
+          }, {
+            type: 'NumberLiteral',
+            value: '2'
+          }]
+        }]
+      }
+    }]
+  };
+} if (input == '(subtract 3 (multiply 2 7))') {
+  var newAst = {
+    type: 'Program',
+    body: [{
+      type: 'ExpressionStatement',
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'subtract'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '3'
+        }, {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'multiply'
+          },
+          arguments: [{
+            type: 'NumberLiteral',
+            value: '2'
+          }, {
+            type: 'NumberLiteral',
+            value: '7'
+          }]
+        }]
+      }
+    }]
+  };
+} if (input == '(multiply 7 (divide 8 2))') {
+  var newAst = {
+    type: 'Program',
+    body: [{
+      type: 'ExpressionStatement',
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'multiply'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '7'
+        }, {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'divide'
+          },
+          arguments: [{
+            type: 'NumberLiteral',
+            value: '8'
+          }, {
+            type: 'NumberLiteral',
+            value: '2'
+          }]
+        }]
+      }
+    }]
+  };
+} if (input == '(Multiply 9 (Add 3 2))') {
+  var newAst = {
+    type: 'Program',
+    body: [{
+      type: 'ExpressionStatement',
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'Multiply'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '9'
+        }, {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: 'Add'
+          },
+          arguments: [{
+            type: 'NumberLiteral',
+            value: '3'
+          }, {
+            type: 'NumberLiteral',
+            value: '2'
+          }]
+        }]
+      }
+    }]
+  };
+}
+
+// ----------------------- ASSERT COMPILER CHECKING ----------------------- //
+
 assert.deepStrictEqual(tokenizer(input), tokens, 'Tokenizer should turn `input` string into `tokens` array');
+assert.deepStrictEqual(parser(tokens), ast, 'Parser should turn `tokens` array into `ast`');
+assert.deepStrictEqual(transformer(ast), newAst, 'Transformer should turn `ast` into a `newAst`');
+assert.deepStrictEqual(codeGenerator(newAst), output, 'Code Generator should turn `newAst` into `output` string');
+assert.deepStrictEqual(compiler(input), output, 'Compiler should turn `input` into `output`');
+
 
 console.log('Accepted');
